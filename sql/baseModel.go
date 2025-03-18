e849ee5f46b7b24dfb3719f6df134eca26159ac3
jiangf0775 <jiangf0775@outlook.com>
2025-03-18 20:56:15 +0800

    去除SqlBuilding

--- sql/baseModel.go
+++ sql/baseModel.go
@@ -3,6 +3,7 @@ package sql
 import (
 	"context"
 	"database/sql"
+	"github.com/Masterminds/squirrel"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
@@ -11,14 +12,14 @@ type BaseModel[TEntity comparable] interface {
 	Insert(ctx context.Context, data *TEntity) (sql.Result, error)
 	FindOne(ctx context.Context, id int64) (*TEntity, error)
 	Update(ctx context.Context, data *TEntity) error
-	FindSum(ctx context.Context, builder SqlBuilder, field string) (float64, error)
-	FindCount(ctx context.Context, builder SqlBuilder, field string) (int64, error)
-	FindAll(ctx context.Context, builder SqlBuilder, orderBy string) ([]*TEntity, error)
-	FindPageListByPage(ctx context.Context, builder SqlBuilder, page, pageSize int64, orderBy string) ([]*TEntity, error)
-	FindPageListByPageWithTotal(ctx context.Context, builder SqlBuilder, page, pageSize int64, orderBy string) ([]*TEntity, int64, error)
-	FindPageListByIdDESC(ctx context.Context, builder SqlBuilder, preMinId, pageSize int64) ([]*TEntity, error)
-	FindPageListByIdASC(ctx context.Context, builder SqlBuilder, preMaxId, pageSize int64) ([]*TEntity, error)
+	FindSum(ctx context.Context, builder squirrel.SelectBuilder, field string) (float64, error)
+	FindCount(ctx context.Context, builder squirrel.SelectBuilder, field string) (int64, error)
+	FindAll(ctx context.Context, builder squirrel.SelectBuilder, orderBy string) ([]*TEntity, error)
+	FindPageListByPage(ctx context.Context, builder squirrel.SelectBuilder, page, pageSize int64, orderBy string) ([]*TEntity, error)
+	FindPageListByPageWithTotal(ctx context.Context, builder squirrel.SelectBuilder, page, pageSize int64, orderBy string) ([]*TEntity, int64, error)
+	FindPageListByIdDESC(ctx context.Context, builder squirrel.SelectBuilder, preMinId, pageSize int64) ([]*TEntity, error)
+	FindPageListByIdASC(ctx context.Context, builder squirrel.SelectBuilder, preMaxId, pageSize int64) ([]*TEntity, error)
 	Trans(ctx context.Context, fn func(ctx context.Context, session sqlx.Session) error) error
-	SelectBuilder() SqlBuilder
+	SelectBuilder() squirrel.SelectBuilder
 	Delete(ctx context.Context, id int64) error
 }
